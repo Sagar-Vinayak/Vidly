5cd765148dfa1a73b8d17be5238f8bc6f1501ef1
Sagar Vinayak <sagar.vinayak@nagarro.com>
2020-04-07 22:59:45 +0530

    First commit

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+using AutoMapper.Mappers;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        private ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new CustomerFormViewModel()
+            {
+                Customer = new Customer(),
+                MembershipTypes = membershipTypes
+            };
+            return View("CustomerForm",viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(Customer customer)
+        {
+            if(!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm",viewModel);
+            }
+
+            if(customer.Id==0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDB = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDB.Name = customer.Name;
+                customerInDB.BirthDate = customer.BirthDate;
+                customerInDB.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+                customerInDB.MembershipTypeId = customer.MembershipTypeId;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Customers");
+        }
+        public ActionResult Edit(int Id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == Id);
+            if (customer == null)
+                return HttpNotFound();
+            var viewModel = new ViewModels.CustomerFormViewModel()
+            { Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            return View("CustomerForm",viewModel);
+        }
+        public ActionResult Index()
+        {
+            var customers = _context.Customers.Include(c=>c.MembershipType).ToList();
+            return View(customers);
+        }
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+            return View(customer);
+        }
+        
+    }
+}
---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+
+        private ApplicationDbContext _context;
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        public ActionResult Index()
+        {
+            var movies = _context.Movies.Include("Genre").ToList();
+            return View(movies);
+        }
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include("Genre").SingleOrDefault(c => c.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+            return View(movie);
+        }
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name="Shrek!"};
+            var movies = new List<Movie>
+            {   new Movie{ Name="Breaking Bad"},
+                new Movie{Name="Life of Pie"},
+                new Movie{Name="Contagion"}
+            };
+            var customers = new List<Customer>
+            {
+                new Customer{Id=1, Name="Customer1"},
+                new Customer{Id=2, Name="Customer2"}
+            };
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movies = movies,
+                Customers = customers
+            };
+            return View(viewModel);
+        }
+
+        private List<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie{Id=1,Name="Breaking Bad"},
+                new Movie{Id=2,Name="Life of Pie"},
+                new Movie{Id=3,Name="Contagion"}
+            };
+        }
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year+" / "+month);
+        }
+    }
+}
